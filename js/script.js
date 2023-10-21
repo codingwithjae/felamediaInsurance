603986545c32e0c4a42d14ce5588a21079116571
Johander <johandercampos@outlook.com>
2023-10-21 14:42:14 -0300

    Main page and form page almost completed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,47 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Get the form and relevant fields
+  let form = document.querySelector(".contact-form");
+  let emailField = form.querySelector("#email");
+  let errorMessage = emailField.nextElementSibling;
+  let errorIcon = emailField.nextElementSibling.nextElementSibling;
 
+  // Email validation function
+  function validateEmail() {
+    let email = emailField.value.trim();
+    let pattern = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zAZ0-9-.]+$/;
 
-function redirectToThankYou() {
-    window.location.href = "https://mariejoemj.my.canva.site/agradecimiento";
+    if (pattern.test(email)) {
+      errorMessage.textContent = "";
+      emailField.classList.remove("error");
+      errorIcon.style.display = "none";
+      return true;
+    } else {
+      errorMessage.textContent = "Formato de correo no válido";
+      errorMessage.style.color = "#FF6F5B";
+      emailField.classList.add("error");
+      return false;
+    }
   }
-  
+
+  // Form submit event
+  form.addEventListener("submit", function (event) {
+    if (!validateEmail()) {
+      // If the email is not valid, prevent form submission
+      event.preventDefault();
+    } else {
+      // Email is valid, redirect to thank you page
+      redirectToThankYou();
+    }
+  });
+
+  // Email field input event
+  emailField.addEventListener("input", function () {
+    validateEmail();
+  });
+});
+
+
+
+
+
+
