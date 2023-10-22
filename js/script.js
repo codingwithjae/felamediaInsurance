407830f5763ff013c1a81a6bb97a38ab9f374ce4
Johander <johandercampos@outlook.com>
2023-10-21 23:20:18 -0300

    new overall changes

--- js/script.js
+++ js/script.js
@@ -1,22 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Cargar el encabezado con JavaScript
-  const headerContainer = document.createElement("div");
-  headerContainer.id = "header-container";
-
-  fetch("header.html") // Carga el contenido del archivo header.html
-    .then((response) => response.text()) // Convierte la respuesta a texto
+  // Load the header content with JavaScript
+  fetch("header-template.html") // Load the content from the header-template.html file
+    .then((response) => response.text()) // Convert the response to text
     .then((headerHtml) => {
-      headerContainer.innerHTML = headerHtml; // Agrega el contenido al contenedor
+      // Add the content of the header directly to the beginning of the document body
+      const body = document.querySelector("body");
+      body.insertAdjacentHTML("afterbegin", headerHtml);
     })
     .catch((error) => {
-      console.error("Error al cargar el encabezado:", error);
+      console.error("Error while loading the header:", error);
     });
 
-  // Agregar el encabezado al comienzo del cuerpo del documento
-  const body = document.querySelector("body");
-  body.insertBefore(headerContainer, body.firstChild);
-
-  // Función para validar el correo electrónico
+  // Function to validate the email
   function validateEmail() {
     const form = document.querySelector(".contact-form");
     const emailField = form.querySelector("#email");
@@ -25,38 +20,33 @@ document.addEventListener("DOMContentLoaded", function () {
     const pattern = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+/;
 
     if (pattern.test(email)) {
-      // Correo electrónico válido, borrar mensaje de error y retornar true
-      errorMessage.textContent = ""; // Borrar el mensaje en la página
+      // Valid email, clear error message and return true
+      errorMessage.textContent = ""; // Clear the message on the page
       return true;
     } else {
-      // Correo electrónico inválido, mostrar un mensaje de error en el elemento <span>
-      errorMessage.textContent = "Por favor, ingrese un correo electrónico válido";
+      // Invalid email, show an error message in the <span> element
+      errorMessage.textContent = "Please enter a valid email";
       emailField.classList.add("error");
 
       return false;
     }
   }
 
-  // Evento de envío del formulario
+  // Form submission event
   const form = document.querySelector(".contact-form");
   form.addEventListener("submit", function (event) {
     if (!validateEmail()) {
-      // Si el correo electrónico no es válido, evitar el envío del formulario
+      // If the email is not valid, prevent form submission
       event.preventDefault();
     } else {
-      // Correo electrónico válido, redirigir a la página de agradecimiento
+      // Valid email, redirect to the thank you page
       redirectToThankYou();
     }
   });
 
-  // Evento de entrada en el campo de correo electrónico
+  // Email field input event
   const emailField = form.querySelector("#email");
   emailField.addEventListener("input", function () {
     validateEmail();
   });
-
-  // Otras funciones y código de tu script principal
-  function redirectToThankYou() {
-    // Código para redirigir a la página de agradecimiento
-  }
 });
